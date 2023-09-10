f500989abc589ee5ef9256b202d8bce60e055321
travelinsuranceweb <72575674+travelinsuranceweb@users.noreply.github.com>
2023-09-11 06:24:33 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -21,10 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="inventory-buybacks-why-do-corporations.html"}
-if(x=="2"){xxx="what-occurs-when-firm-buys-again-shares.html"}
-if(x=="3"){xxx="4-causes-traders-like-buybacks.html"}
-if(x=="4"){xxx="dividend-vs-buyback-what-is-distinction.html"}a_to_ve[i].href="https://web.samehada.my.id/2022/11/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="11-web-site-growth-providers-to-have.html"}
+if(x=="2"){xxx="11-web-site-growth-providers-to-have.html"}
+if(x=="3"){xxx="11-web-site-growth-providers-to-have.html"}
+if(x=="4"){xxx="11-web-site-growth-providers-to-have.html"}a_to_ve[i].href="https://siuzang.blogspot.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
